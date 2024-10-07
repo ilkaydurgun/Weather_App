c7a6b037254c2506fa8ea7bb6c8ff4ee6da80da9
Ilkay Durgun <ilkydrgun99@gmail.com>
2024-10-07 11:43:00 +0300

    Program.cs Update

--- Program.cs
+++ Program.cs
@@ -7,36 +7,29 @@ using Weather_App.Context;
 using Weather_App.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
-//Servisleri ekleme
+
+// Servisleri ekleme
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); // Düzeltildi: "DefualtConnection" yerine "DefaultConnection"
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<WeathersController>(); // WeathersController'i DI'ye ekleme
 builder.Services.AddHostedService<WeatherUpdateService>(); // Arka plan hizmetini ekleme
 
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefualtConnection"));
-});
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}"); 
 
 app.Run();
